74fb85d2e9155ae1bf25314975a2f12292a40d63
merv-e <34655454+merv-e@users.noreply.github.com>
2024-06-12 23:06:04 +0200

    FormInput component is simplified and splitted into more components. an index file is created to ease the process of imports and exports.

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,43 +1,17 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 
-const FormInput = ({ input_data, button_data }) => {
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleSubmit = () => {};
-
+const FormInput = ({ name, defaultValue, label, type }) => {
   return (
-    <main className="hero min-h-screen bg-base-200">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          {isLoading ? (
-            <span className="loading loading-spinner loading-sm"></span>
-          ) : (
-            <form onSubmit={handleSubmit} className="flex flex-col gap-5">
-              {input_data.map((data) => (
-                <div key={data.id}>
-                  <label className="input input-bordered flex items-center gap-2">
-                    {data.label_name}
-                    <input
-                      type={data.type}
-                      className="grow"
-                      placeholder={data.placeholder}
-                    />
-                  </label>
-                </div>
-              ))}
-              {button_data.map((data) => (
-                <div className="flex flex-col justify-center gap-2" key={data.id}>
-                  <Link to="/" className="btn btn-primary">
-                    {data.btn_name}
-                  </Link>
-                </div>
-              ))}
-            </form>
-          )}
-        </div>
-      </div>
-    </main>
+    <label className="input input-bordered flex items-center gap-2">
+      {label}
+      <input
+        type={type}
+        name={name}
+        className="grow"
+        placeholder={name}
+        defaultValue={defaultValue}
+      />
+    </label>
   );
 };
 
