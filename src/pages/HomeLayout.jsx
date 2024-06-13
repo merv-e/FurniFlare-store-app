0f55f1e54730345b5bd98963c99dc2d9bb409b68
merv-e <34655454+merv-e@users.noreply.github.com>
2024-06-13 15:14:25 +0200

    Header component is built and added to HomeLayout.

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Header } from "../components";
 
 const HomeLayout = () => {
   return (
-    <div>
-      <nav>
-        <span className="text-4xl text-secondary">VividDwell</span>
-      </nav>
+    <>
+      <Header />
       <section className="align-element py-20">
         <Outlet />
       </section>
-    </div>
+    </>
   );
 };
 
