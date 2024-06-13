cc55495d43875d9ffeda1efa64772bd2ea96a4e4
merv-e <34655454+merv-e@users.noreply.github.com>
2024-06-13 14:53:43 +0200

    a very similar page to Login page is created  which is the Register page.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,40 @@
-import React from "react";
-import FormInput from "../components/FormInput";
-import { register_btn, register_form_input } from "../data";
+import React, { useState } from "react";
+import { FormInput, SubmitBtn } from "../components";
+import { Form, Link } from "react-router-dom";
 
 const Register = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <FormInput input_data={register_form_input} button_data={register_btn} />
+    <section className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          {isLoading ? (
+            <span className="loading loading-spinner loading-lg"></span>
+          ) : (
+            <Form
+              method="POST"
+              className="card w-96 p-14 flex flex-col gap-y-5 shadow-2xl"
+            >
+              <h3 className="text-3xl font-bold mb-6">Register</h3>
+              <FormInput type="username" label="username" name="username" />
+              <FormInput type="email" label="email" name="identifier" />
+              <FormInput type="password" label="password" name="password" />
+              <SubmitBtn text="Register" />
+              <p className="font-bold">
+                Already a member?
+                <Link
+                  to="/login"
+                  className="ml-2 link link-hover link-primary"
+                >
+                  Login
+                </Link>
+              </p>
+            </Form>
+          )}
+        </div>
+      </div>
+    </section>
   );
 };
 
